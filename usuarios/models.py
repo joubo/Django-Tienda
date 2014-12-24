d36b43f183d407bd368acbea97a64e6cb7c913ab
Jordi Bordoy <i22bonij@uco.es>
2014-12-24 17:27:24 +0100

    desarrollo registro

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,7 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+
 
 # Create your models here.
 
-User.add_to_class('direccion', models.CharField(max_length = 40)),
-User.add_to_class('telefono', models.CharField(max_length=9)),
+class Usuario(models.Model):
+
+	login_id  =  models.CharField(max_length=30)
+	nombre = models.CharField(max_length=30, blank=True, default='')
+	apellidos  = models.CharField(max_length=30, blank=True, default='')
+	email = models.EmailField()
+	password = models.CharField(max_length=20)
+	dni = models.CharField(max_length=9)
+	direccion = models.CharField(max_length=50)
+	created_time = models.DateTimeField(auto_now_add=True)
+
+
+class UsuarioForm(ModelForm):
+    
+	class Meta:
+		model = Usuario
+		exclude = [] 
